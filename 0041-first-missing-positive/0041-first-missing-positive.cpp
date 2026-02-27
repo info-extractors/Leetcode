b6cff54a9e24453e8499804de676107cdd9d7bd0
Reshab Sharma <154262791+info-extractors@users.noreply.github.com>
2026-02-27 07:51:52 +0530

    Time: 3 ms (53.61%), Space: 55 MB (85.03%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -5,12 +5,13 @@ public:
         int n = nums.size();
 
         for(int i = 0;i < n;i++){
-            while(nums[i] >= 1 && nums[i] < n && nums[nums[i] - 1] != nums[i]){
-                swap(nums[i],nums[nums[i] - 1]);
+
+            while(nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
+                swap(nums[nums[i] - 1],nums[i]);
             }
         }
 
-        for(int i = 0;i < nums.size();i++){
+        for(int i = 0;i < n;i++){
             if (nums[i] != i + 1) return i + 1;
         }
 
