43dc84abde11e0c443c65b67d36b686f01e2c428
Reshab Sharma <154262791+info-extractors@users.noreply.github.com>
2026-03-18 13:18:28 +0530

    Time: 14 ms (23.12%), Space: 7.8 MB (36.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
 
-    int cal(int n,vector<int> &dp){
+    int cal(int n){
         if (n == 0) return 0;
         if (n == 1) return 1;
 
-        if (dp[n] != -1) return dp[n];
+        // if (dp[n] != -1) return dp[n];
 
-        return dp[n] = fib(n - 1) + fib(n - 2);
+        return fib(n - 1) + fib(n - 2);
     }
     int fib(int n) {
-        vector<int> dp(n + 1,-1);
-        return cal(n,dp);
+        // vector<int> dp(n + 1,-1);
+        return cal(n);
     }
 };
