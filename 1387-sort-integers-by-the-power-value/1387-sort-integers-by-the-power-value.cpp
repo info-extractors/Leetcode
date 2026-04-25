d1da585ee3daa1590b90218b8b3748a8975f68bd
Reshab Sharma <154262791+info-extractors@users.noreply.github.com>
2026-04-26 03:11:55 +0530

    Time: 351 ms (6.4%), Space: 11.2 MB (74.56%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
 
 
-    int getPower(int n,unordered_map<int,int> &dp){
+    int getPower(int n){
 
         if (n == 1) return 0;
 
-        if (dp.count(n)) return dp[n];
-
         if (n & 1){
-            return dp[n] = 1 + getPower(3 * n + 1,dp);
+            return 1 + getPower(3 * n + 1);
         }
 
-        return dp[n] = 1 + getPower(n / 2,dp);
+        return 1 + getPower(n / 2);
     }
 
     int getKth(int lo, int hi, int k) {
 
-        unordered_map<int,int> dp;
-
         vector<pair<int,int>> powers;
 
         for(int i = lo;i <= hi;i++){
-            powers.push_back({getPower(i,dp),i});
+            powers.push_back({getPower(i),i});
         }
 
         for(auto i : powers) cout << i.first <<" "<<i.second << endl;
