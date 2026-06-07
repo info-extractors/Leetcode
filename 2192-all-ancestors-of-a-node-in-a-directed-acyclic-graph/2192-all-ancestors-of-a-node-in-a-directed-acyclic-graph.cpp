c9752e0454ac78fd459eafd1ec4cf96f79918c78
Reshab Sharma <154262791+info-extractors@users.noreply.github.com>
2026-06-07 21:33:45 +0530

    Time: 362 ms (19.38%), Space: 146.2 MB (37.22%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        
+        vector<int> inDegree(n);
+        vector<vector<int>> graph(n);
+
+        for(vector<int> edge : edges){
+
+            int u = edge[0];
+            int v = edge[1];
+
+            inDegree[v]++;
+            graph[u].push_back(v);
+        }
+
+        queue<int> q;
+
+        for(int i = 0;i < inDegree.size();i++){
+            if (inDegree[i] == 0) q.push(i);
+        }
+
+        vector<set<int>> temp_ans(n);
+
+        while(!q.empty()){
+
+            int frontNode = q.front();q.pop();
+
+            for(int neighbor : graph[frontNode]){
+
+                temp_ans[neighbor].insert(frontNode);
+
+                for(auto ancestors:temp_ans[frontNode]){
+                    temp_ans[neighbor].insert(ancestors);
+                }
+
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) q.push(neighbor);
+            }
+        }
+
+        vector<vector<int>> ans(n);
+
+        for(size_t i{};i < n;i++){
+            ans[i] = vector<int>(temp_ans[i].begin(),temp_ans[i].end());
+        }
+
+        return ans;
+    }
+};
+
+// even if the indegree is not zero the node is still the ancestor of the node 
+//so we have to do it
